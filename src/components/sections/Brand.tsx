08cc07a53d7d06c4b022f58b39382f03d95a8393
Alisha Batham <alishabatham2@gmail.com>
2026-09-22 00:06:22 +0530

    Update app icon, favicon, and brand logo

--- src/components/sections/Brand.tsx
+++ src/components/sections/Brand.tsx
@@ -1,9 +1,9 @@
-import onewinqLogo from '@assets/onewinq_black_logo_1790007174259.png';
+import onewinqLogo from '@assets/onewinq_app_icon.png';
 
 export function Brand() {
   return (
     <a className="brand" href="#top" aria-label="OneWinq home" data-testid="link-brand">
-      <img className="brand-logo" src={onewinqLogo} alt="OneWinq logo" />
+      <img className="brand-logo" src={onewinqLogo} alt="OneWinq logo" style={{ height: '38px', width: '38px', borderRadius: '50%', objectFit: 'cover' }} />
     </a>
   );
 }
